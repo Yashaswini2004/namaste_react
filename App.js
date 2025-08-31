388d4a85163bac46de27959f59d58fb68766472a
Yashaswini2004 <yashaswinic31@gmail.com>
2025-08-31 19:45:14 +0530

    first b

---
+++ App.js
@@ -0,0 +1,5 @@
+ const heading=React.createElement("h1",{id:"title",className:"ti"},"Name")
+        const content=React.createElement("p",{},"Yashaswini")
+        const div=React.createElement("div",{id:"container"},[heading,content]);
+        const root=ReactDOM.createRoot(document.getElementById("root"));
+        root.render(div)
